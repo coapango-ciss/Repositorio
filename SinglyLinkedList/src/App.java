dbf1c6734196ebf5fc9373aa547c68d55e49a617
coapango-ciss <20233tn218@utez.edu.mx>
2024-10-29 08:15:31 -0600

    codigo completado

--- SinglyLinkedList/src/App.java
+++ SinglyLinkedList/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SinglyLinkedList lista = new SinglyLinkedList(null);
+        lista.insertar(10);
+        lista.insertar(20);
+        lista.insertar(30);
+        System.out.println("-------------------------------------");
+        System.out.println("Buscar");
+        lista.mostrar();
+        lista.buscar(30);
+        System.out.println("-------------------------------------");
+        System.out.println("Eliminar");
+        lista.eliminar(10);
+        lista.eliminar(20);
+        lista.eliminar(30);
     }
 }
--- SinglyLinkedList/src/SinglyLinkedList.java
+++ SinglyLinkedList/src/SinglyLinkedList.java
@@ -1,6 +1,6 @@
 public class SinglyLinkedList {
     Nodo cabeza;
-    public SinglyLinkedList(){
+    public SinglyLinkedList(Nodo cabeza){
         this.cabeza = null;
     }
 
@@ -18,5 +18,49 @@ public class SinglyLinkedList {
     }
     
     public void mostrar(){
+        Nodo temp = cabeza;
+        while(temp != null){
+            System.out.println(temp.getDato() + "->");
+            temp.getSiguiente();
+        }
+        System.out.println("null");
+    }
+    public boolean buscar(Integer valor){
+        if(cabeza == null){
+            System.out.println("La lista está vacía");
+        } else {
+            Nodo temp = cabeza;
+            while (temp != null) {
+                if(temp.getDato() == valor){
+                    System.out.println("Se ha encontrado el nodo");
+                    return true;
+                }
+                temp = temp.getSiguiente();
+            }
+            System.out.println("El nodo con el valor buscado no está en la lista");
+        }
+        return false;
+    }
+    public void eliminar(Integer valor){
+        if(cabeza == null){
+            System.out.println("No se puede eliminar porque la lista está vacía");
+        }
+        if(cabeza.getDato() == valor){
+            cabeza = cabeza.getSiguiente();
+            System.out.println("Se elimino correctamente el valor" + valor);
+            return;
+        }
+        Nodo temp = cabeza;
+        Nodo anterior = null;
+        while(temp != null && temp.getDato() != valor){
+            anterior = temp;
+            temp =  temp.getSiguiente();
+        }
+        if(temp == null){
+            System.out.println("No se encontró el valor");
+            return;
+        }
+        anterior.setSiguiente(temp.getSiguiente());
+        System.out.println("Se elimino correctamente el valor" + valor);
     }
 }
