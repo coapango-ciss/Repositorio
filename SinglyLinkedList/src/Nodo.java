4c60ef14239b71bbaeb261d76cb8654a9a14224c
coapango-ciss <20233tn218@utez.edu.mx>
2024-10-29 07:51:04 -0600

    Actualización

--- SinglyLinkedList/src/Nodo.java
+++ SinglyLinkedList/src/Nodo.java
@@ -1,3 +1,23 @@
 public class Nodo {
-    
+    public Integer dato;
+    private Nodo siguiente;
+    public Nodo(Integer dato){
+        this.dato = dato;
+    }
+
+    public Integer getDato(){
+        return dato;
+    }
+
+    public void setDato(Integer dato){
+        this.dato = dato;
+    }
+
+    public Nodo getSiguiente(){
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente){
+        this.siguiente = siguiente;
+    }
 }
--- SinglyLinkedList/src/SinglyLinkedList.java
+++ SinglyLinkedList/src/SinglyLinkedList.java
@@ -1,3 +1,22 @@
 public class SinglyLinkedList {
+    Nodo cabeza;
+    public SinglyLinkedList(){
+        this.cabeza = null;
+    }
+
+    public void insertar(Integer dato){
+        Nodo nuevoNodo = new Nodo(dato);
+        if(cabeza == null){
+            cabeza = nuevoNodo;
+        } else {
+            Nodo temp = cabeza;
+            while(temp.getSiguiente() != null){
+                temp =  temp.getSiguiente();
+            }
+            temp.setSiguiente(nuevoNodo);
+        }
+    }
     
+    public void mostrar(){
+    }
 }
